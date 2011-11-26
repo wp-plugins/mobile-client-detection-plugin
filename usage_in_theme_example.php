a48866375d0cf5459ef09eb3600719f0feef18a1
syslogic <syslogic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-26 13:42:27 +0000

    updated once more
    git-svn-id: https://plugins.svn.wordpress.org/mobile-client-detection-plugin/trunk@467729 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- usage_in_theme_example.php
+++ usage_in_theme_example.php
@@ -1,61 +1,57 @@
 <?php
-	/* Template Name: Mobile Client Detection Example */
-	/*
-		this is an example how provided query_var 'platform' and 'browser' can be used in your theme
-		
-		- place this code into your theme (e.g. functions.php, index.php, header.php)
-		  the concrete placement merely depends on the concrete situation
-		
-		The var $output can enable/disable debug output, possible values: true/false
-		
-		The actual switching doesn't happen here, the code just processes the a WP query_var.
-		
-	*/
+/* Template Name: Mobile Client Detection Example */
+/*
+	This is a basic example how query_var 'platform' and 'browser' can be used in a theme
 	
-	/* if the query_var even exists */
-	if(get_query_var('platform')){
+	The actual switching doesn't happen here, the code just processes the a WP query_var.
+*/
+	
+/* if the query_var 'platform' even exists */
+if(get_query_var('platform')){
+	$platform = get_query_var('platform');
+	switch($platform){
 		
-		$platform = get_query_var('platform');
+		/* phones */
+		case 'mobile':		$tag = 'Mobile';break;
+		case 'android 1':	
+		case 'android 2':	$tag = 'Android (Phone)';
+											break;
+		case 'android 3':	
+		case 'android 4':	$tag = 'Android (Tablet)';
+											break;
+		case 'blackberry':$tag = 'BlackBerry';break;
+		case 'iphone':		
+		case 'ipod':			$tag = 'Apple (Phone)';break;
+		case 'ipad':			$tag = 'Apple (Tablet)';break;
+		case 'iemobile':	$tag = 'mobile IE';break;
+		case 'webos':			$tag = 'webOS';break;
+		case 'kindle':		$tag = 'Kindle';break;
+		case 'palm':			$tag = 'Palm';break;
 		
-		switch($platform){
-			
-			/* phones */
-			case 'mobile':		$tag = 'Mobile';break;
-			case 'android 1':	
-			case 'android 2':	$tag = 'Android (Phone)';
-												break;
-			case 'android 3':	
-			case 'android 4':	$tag = 'Android (Tablet)';
-												break;
-			case 'blackberry':$tag = 'BlackBerry';break;
-			case 'iphone':		
-			case 'ipod':			$tag = 'Apple (Phone)';break;
-			case 'ipad':			$tag = 'Apple (Tablet)';break;
-			case 'iemobile':	$tag = 'mobile IE';break;
-			case 'webos':			$tag = 'webOS';break;
-			case 'kindle':		$tag = 'Kindle';break;
-			case 'palm':			$tag = 'Palm';break;
-			
-			/* desktop clients */
-			case 'windows':
-			case 'win64':
-			case 'macintosh':
-			case 'ppx mac os x':
-			case 'intel mac os x':
-			case 'googlebot':
-			case 'desktop':		$tag = 'Desktop';break;
-			
-			/* this case will not happen - since the query_var defaults to desktop */
-			default:					$tag = $platform;break;
-		}
+		/* desktop clients */
+		case 'windows':
+		case 'win64':
+		case 'macintosh':
+		case 'ppx mac os x':
+		case 'intel mac os x':
+		case 'googlebot':
+		case 'desktop':		$tag = 'Desktop';break;
 		
-		if(get_query_var('browser')){
-			$browser = get_query_var('browser');
-			switch($browser){
-				
-				/* ... */
-				
-			}
-		}
+		/* this case will not happen - since the query_var defaults to desktop */
+		default:					$tag = $platform;break;
 	}
+}
+/* if the query_var 'browser' even exists */
+if(get_query_var('browser')){
+	$browser = get_query_var('browser');
+	switch($browser){
+		
+		/* ... */
+		
+	}
+}
+
+get_header();
+echo '<div>'.$tag.'</div>';
+get_footer();
 ?>
