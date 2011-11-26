fba84e986e5cf4d85f90521c16e55a85e6b0ef97
syslogic <syslogic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-26 10:08:00 +0000

    changed encoding to UTF-8
    git-svn-id: https://plugins.svn.wordpress.org/mobile-client-detection-plugin/trunk@467658 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mobile_client_detection.php
+++ mobile_client_detection.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Mobile Client Detection Plug-in
 	Plugin URI: http://plugins.codefx.biz/mobile-client-detection.zip
-	Description: The Mobile Client Detection Plug-in provides query_vars 'platform' & 'browser' for simply switching the layout within your theme (requires editing template files). It can also be very helpful when it’s required to load different versions of CSS/JS code.
+	Description: The Mobile Client Detection Plug-in provides query_vars 'platform' & 'browser' for simply switching the layout within your theme (requires editing template files). It can also be very helpful when itâ€™s required to load different versions of CSS/JS code.
 	Author: Martin Zeitler
 	Version: 0.5
 	Tags: plugin, mobile, theme, detect, query_var, layout, switch
